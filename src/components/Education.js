ab007e8b3d9cc57775971f55bd8676c5df2bb421
threehundo <samuelbaik87@gmail.com>
2021-02-17 12:47:53 -0800

    Education converted to Hook

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,132 +1,108 @@
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-// import Row from "react-bootstrap/Row";
-// import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import React from "react";
 
-class Education extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Education = (props) => {
+  const hideDiv = (props) => (props ? "hide" : "");
 
-  render() {
-    const {
-      education,
-      toggleInput,
-      educationInput,
-      handleChange,
-      handleDelete,
-      educationForm,
-      handleSubmit,
-    } = this.props;
+  const hideInput = (props) => (props ? "" : "hide");
 
-    console.log(education);
-    console.log("Type: " + typeof eduacation);
-
-    const view = education.map((info) => {
-      let index = education.indexOf(info);
-      const schools = Object.entries(info).map((item, index) => (
-        <div key={index}>
-          <div>
-            <span>{item[0]}: </span>
-            {item[1]}
-          </div>
+  const view = props.education.map((info) => {
+    let index = props.education.indexOf(info);
+    const schools = Object.entries(info).map((item, index) => (
+      <div key={index}>
+        <div>
+          <span>{item[0]}: </span>
+          {item[1]}
         </div>
-      ));
-      return (
-        <Container className="outline" fluid="sm" key={index}>
-          {schools}
-          <Button
-            className="schoolBtn"
-            id={index}
-            onClick={handleDelete}
-            variant="outline-warning"
-          >
-            ❌
-          </Button>
-        </Container>
-      );
-    });
-
-    const EduArr = (
-      <Form id="educationInput" onSubmit={handleSubmit}>
-        <Form.Group>
-          <Form.Label id="school">School: </Form.Label>
-          <Form.Control
-            name="school"
-            onChange={handleChange}
-            value={educationForm.school}
-          />
-          <Form.Text />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label id="city">City: </Form.Label>
-          <Form.Control
-            name="city"
-            onChange={handleChange}
-            value={educationForm.city}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label id="from">From: </Form.Label>
-          <Form.Control
-            name="from"
-            onChange={handleChange}
-            value={educationForm.from}
-          />
-          <Form.Text />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label id="to">To: </Form.Label>
-          <Form.Control
-            name="to"
-            onChange={handleChange}
-            value={educationForm.to}
-          />
-          <Form.Text />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label id="degree">Degree: </Form.Label>
-          <Form.Control
-            name="degree"
-            onChange={handleChange}
-            value={educationForm.degree}
-          />
-          <Form.Text />
-        </Form.Group>
-        <Button type="submit" value="Submit" variant="outline-success">
-          ✅
-        </Button>
-      </Form>
-    );
-
+      </div>
+    ));
     return (
-      <Container id="educationContainer" className="offSetBtm">
-        <div className={this.hideDiv(educationInput)}>
-          {view}
-          <Button
-            className="schoolBtn"
-            id="addSchoolBtn"
-            variant="outline-success"
-            onClick={() => toggleInput("educationInput")}
-          >
-            ➕
-          </Button>
-        </div>
-        <Container
-          id="educationForm"
-          className={this.hideInput(educationInput)}
+      <Container className="outline" fluid="sm" key={index}>
+        {schools}
+        <Button
+          className="schoolBtn"
+          id={index}
+          onClick={props.handleDelete}
+          variant="outline-warning"
         >
-          {EduArr}
-        </Container>
+          ❌
+        </Button>
       </Container>
     );
-  }
+  });
 
-  hideDiv = (props) => (props ? "hide" : "");
+  const EduArr = (
+    <Form id="educationInput" onSubmit={props.handleSubmit}>
+      <Form.Group>
+        <Form.Label id="school">School: </Form.Label>
+        <Form.Control
+          name="school"
+          onChange={props.handleChange}
+          value={props.educationForm.school}
+        />
+        <Form.Text />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label id="city">City: </Form.Label>
+        <Form.Control
+          name="city"
+          onChange={props.handleChange}
+          value={props.educationForm.city}
+        />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label id="from">From: </Form.Label>
+        <Form.Control
+          name="from"
+          onChange={props.handleChange}
+          value={props.educationForm.from}
+        />
+        <Form.Text />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label id="to">To: </Form.Label>
+        <Form.Control
+          name="to"
+          onChange={props.handleChange}
+          value={props.educationForm.to}
+        />
+        <Form.Text />
+      </Form.Group>
+      <Form.Group>
+        <Form.Label id="degree">Degree: </Form.Label>
+        <Form.Control
+          name="degree"
+          onChange={props.handleChange}
+          value={props.educationForm.degree}
+        />
+        <Form.Text />
+      </Form.Group>
+      <Button type="submit" value="Submit" variant="outline-success">
+        ✅
+      </Button>
+    </Form>
+  );
 
-  hideInput = (props) => (props ? "" : "hide");
-}
+  return (
+    <Container id="educationContainer" className="offSetBtm">
+      <div className={hideDiv(props.educationInput)}>
+        {view}
+        <Button
+          className="schoolBtn"
+          id="addSchoolBtn"
+          variant="outline-success"
+          onClick={() => props.toggleInput("educationInput")}
+        >
+          ➕
+        </Button>
+      </div>
+      <Container id="educationForm" className={hideInput(props.educationInput)}>
+        {EduArr}
+      </Container>
+    </Container>
+  );
+};
 
 export default Education;
